6604c630c8a3e6dcd7f4d475237ddc5152e362e1
ybmac <ybmac@ybmacdeimac.local>
2017-12-20 11:01:10 +0800

    Initial Commit

---
+++ statisticalApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  statisticalApp
+//
+//  Created by ybmac on 2017/12/20.
+//  Copyright © 2017年 ybmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ statisticalApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  statisticalApp
+//
+//  Created by ybmac on 2017/12/20.
+//  Copyright © 2017年 ybmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
